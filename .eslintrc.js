fb4635df2c499c402ed3af65f3a909c9da3a2428
bjoernsanders <bjoern.sanders@pflegix.de>
2023-01-17 14:22:15 +0100

    Revert "install node and eslint"
    This reverts commit ac51864b14d6255282b0bea10864544101a59a20.

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    parserOptions: {
-      ecmaVersion: 2018,
-    },
-    extends: ["eslint:recommended"],
-    plugins: [],
-    rules: {},
-    ignorePatterns: ['.eslintrc.js',]
-  };
