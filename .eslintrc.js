ac51864b14d6255282b0bea10864544101a59a20
bjoernsanders <bjoern.sanders@pflegix.de>
2023-01-17 14:14:41 +0100

    install node and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    parserOptions: {
+      ecmaVersion: 2018,
+    },
+    extends: ["eslint:recommended"],
+    plugins: [],
+    rules: {},
+    ignorePatterns: ['.eslintrc.js',]
+  };
